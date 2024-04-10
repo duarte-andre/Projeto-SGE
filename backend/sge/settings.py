377cec40e2ebc2ab3341bf6e08d61eed796a3924
rubens <127998997+duarte-andre@users.noreply.github.com>
2024-04-09 22:37:52 -0300

    ataualização dos https no settings

--- backend/sge/settings.py
+++ backend/sge/settings.py
@@ -31,7 +31,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['http://localhost', 'http://127.0.0.1', ]
+CSRF_TRUSTED_ORIGINS = ['http://localhost', 'http://127.0.0.1', 'https://projeto-sge-production.up.railway.app']
 
 
 # Application definition
